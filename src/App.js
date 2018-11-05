aef19fcdf5d582086d0d81a4295ec93997020c20
amnonbb <amnonbb@gmail.com>
2018-11-05 08:49:16 +0200

    ShidurAdmin: new component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
+import ShidurAdmin from "./apps/ShidurApp/ShidurAdmin";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
 // import ShidurGroups from "./apps/ShidurApp/ShidurGroups";
 // import SndmanClient from "./apps/SndmanApp/SndmanClient";
@@ -7,7 +8,7 @@ import 'semantic-ui-css/semantic.min.css';
 // import ShidurUsers from "././apps/ShidurApp/ShidurUsers";
 // import AdminClient from "./apps/AdminApp/AdminClient";
 // import SDIOutClient from "./apps/SDIOutApp/SDIOutClient";
-import VirtualClient from "./apps/VirtualApp/VirtualClient";
+// import VirtualClient from "./apps/VirtualApp/VirtualClient";
 // import VirtualStreaming from "./components/VirtualStreaming";
 
 class App extends Component {
@@ -20,11 +21,12 @@ class App extends Component {
             <Fragment>
                 {/*<Streaming />*/}
                 {/*<ShidurUsers />*/}
+                <ShidurAdmin/>
                 {/*<SDIOutClient />*/}
                 {/*<SndmanClient />*/}
                 {/*<AdminStreaming/>*/}
                 {/*<AdminClient />*/}
-                <VirtualClient />
+                {/*<VirtualClient />*/}
                 {/*<VirtualStreaming/>*/}
                 {/*<ShidurGroups/>*/}
                 {/*<ShidurApp/>*/}
