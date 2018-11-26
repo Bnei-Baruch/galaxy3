9499f29c709bf8a56bcdda19490300f8f13fe965
amnonbb <amnonbb@gmail.com>
2018-11-26 11:18:08 +0200

    Cleanup main App

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import AdminGuest from "./apps/AdminApp/AdminGuest";
-// import GalaxyStream from "./components/GalaxyStream";
+import VirtualClient from "./apps/VirtualApp/VirtualClient";
 // import ShidurAdmin from "./apps/ShidurApp/ShidurAdmin";
 // import SndmanApp from "./apps/SndmanApp/SndmanApp";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
-// import ShidurGroups from "./apps/ShidurApp/ShidurGroups";
 // import GroupClient from "./apps/GroupsApp/GroupClient";
-// import GroupsApp from "./apps/GroupsApp/GroupsApp";
-// import SndmanClient from "./apps/SndmanApp/SndmanClient";
-// import AdminStreaming from "./apps/AdminApp/AdminStreaming";
-// import ShidurUsers from "././apps/ShidurApp/ShidurUsers";
-// import AdminClient from "./apps/AdminApp/AdminClient";
-// import SDIOutUsers from "./apps/SDIOutApp/SDIOutUsers";
 // import SDIOutApp from "./apps/SDIOutApp/SDIOutApp";
-// import VirtualClient from "./apps/VirtualApp/VirtualClient";
-// import VirtualStreaming from "./components/VirtualStreaming";
 
 class App extends Component {
-
-    componentDidMount() {
-    };
 
     render() {
         return (
             <Fragment>
-                {/* <GalaxyStream/> */}
-                {/*<Streaming />*/}
-                {/*<ShidurUsers />*/}
+                <VirtualClient/>
                  {/*<ShidurAdmin/>*/}
-                {/*<SDIOutUsers />*/}
                 {/*<SDIOutApp/>*/}
-                {/*<SndmanClient />*/}
-                {/*<AdminStreaming/>*/}
-                {/*<AdminClient />*/}
-                {/*<VirtualClient />*/}
-                {/*<VirtualStreaming/>*/}
-                {/*<ShidurGroups/>*/}
                  {/*<ShidurApp/>*/}
                  {/*<GroupClient/>*/}
-                 {/*<GroupsApp/>*/}
                  {/*<SndmanApp/>*/}
-                 <AdminGuest/>
             </Fragment>
         );
     }
