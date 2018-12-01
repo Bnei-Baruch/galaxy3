fb864c152fba5520d810f04b85946f4b7337b9af
amnonbb <amnonbb@gmail.com>
2018-12-01 12:16:16 +0200

    SndmanApp: Set forward data state after callback, don't use full_feed property for forward state.

--- src/apps/SndmanApp/SndmanApp.js
+++ src/apps/SndmanApp/SndmanApp.js
@@ -224,10 +224,10 @@ class SndmanApp extends Component {
                 }
             } else if(event === "talking") {
                 let id = msg["id"];
-                Janus.log("User: "+id+" - start talking");
+                Janus.debug("User: "+id+" - start talking");
             } else if(event === "stopped-talking") {
                 let id = msg["id"];
-                Janus.log("User: "+id+" - stop talking");
+                Janus.debug("User: "+id+" - stop talking");
             } else if(event === "destroyed") {
                 // The room has been destroyed
                 Janus.warn("The room has been destroyed!");
