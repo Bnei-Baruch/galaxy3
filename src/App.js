a0d050859b41407975c11aacadbf65de6ea01b09
amnonbb <amnonbb@gmail.com>
2019-08-29 09:46:42 +0300

    Remove old janus.js library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-// import GalaxyApp from "./apps/GalaxyApp";
+import GalaxyApp from "./apps/GalaxyApp";
 // import VirtualStreaming from "./apps/VirtualApp/VirtualStreaming";
 // import GalaxyStream from "./apps/StreamApp/GalaxyStream";
 // import AdminStreaming from "./apps/AdminApp/AdminStreaming";
@@ -9,7 +9,7 @@ import 'semantic-ui-css/semantic.min.css';
 // import SDIOutApp from "./apps/SDIOutApp/SDIOutApp";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
 // import MobileClient from "./apps/MobileApp/MobileClient";
-import SndmanApp from "./apps/SndmanApp/SndmanApp";
+// import SndmanApp from "./apps/SndmanApp/SndmanApp";
 // import GroupClient from "./apps/GroupsApp/GroupClient";
 // import GalaxyStream from "./apps/StreamApp/GalaxyStream";
 // import AdminGuest from "./apps/AdminApp/AdminGuest";
@@ -20,12 +20,12 @@ class App extends Component {
     render() {
         return (
             <Fragment>
-                {/*<GalaxyApp />*/}
+                <GalaxyApp />
                 {/*<VirtualClient/>*/}
                 {/* <ShidurAdmin/>*/}
                 {/*<SDIOutApp/>*/}
-                {/* <ShidurApp/>*/}
-                 <SndmanApp/>
+                 {/*<ShidurApp/>*/}
+                 {/*<SndmanApp/>*/}
                 {/*<GroupClient/>*/}
                 {/* <GalaxyStream/>*/}
                 {/* <AdminGuest/>*/}
