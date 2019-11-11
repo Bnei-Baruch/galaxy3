4972d7718b51d4a2d90312ef6c9c2db6579604f3
amnonbb <amnonbb@gmail.com>
2019-11-11 13:12:59 +0200

    Change apps order in main App

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import GalaxyApp from "./apps/GalaxyApp";
-// import VirtualStreaming from "./apps/VirtualApp/VirtualStreaming";
-// import GalaxyStream from "./apps/StreamApp/GalaxyStream";
-// import AdminStreaming from "./apps/AdminApp/AdminStreaming";
+// import GalaxyApp from "./apps/GalaxyApp";
+// import MobileClient from "./apps/MobileApp/MobileClient";
 // import VirtualClient from "./apps/VirtualApp/VirtualClient";
+// import VirtualStreaming from "./apps/VirtualApp/VirtualStreaming";
+// import GroupClient from "./apps/GroupsApp/GroupClient";
+// import GalaxyStream from "./apps/StreamApp/GalaxyStream";
 // import ShidurAdmin from "./apps/ShidurApp/ShidurAdmin";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
 // import SndmanApp from "./apps/SndmanApp/SndmanApp";
-// import SDIOutApp from "./apps/SDIOutApp/SDIOutApp";
-// import MobileClient from "./apps/MobileApp/MobileClient";
-// import GroupClient from "./apps/GroupsApp/GroupClient";
-// import GalaxyStream from "./apps/StreamApp/GalaxyStream";
+import SDIOutApp from "./apps/SDIOutApp/SDIOutApp";
 // import AdminGuest from "./apps/AdminApp/AdminGuest";
 // import AdminCongress from "./apps/AdminApp/AdminCongress";
+// import AdminStreaming from "./apps/AdminApp/AdminStreaming";
 
 class App extends Component {
 
     render() {
         return (
             <Fragment>
-                <GalaxyApp />
+                {/*<GalaxyApp />*/}
+                {/*  <MobileClient/>*/}
                 {/*<VirtualClient/>*/}
-                {/* <ShidurAdmin/>*/}
-                 {/*<ShidurApp/>*/}
-                 {/*<SndmanApp/>*/}
-                {/*<SDIOutApp/>*/}
+                {/* <VirtualStreaming/>*/}
                 {/*<GroupClient/>*/}
                 {/* <GalaxyStream/>*/}
+                {/* <ShidurAdmin/>*/}
+                {/* <ShidurApp/>*/}
+                {/* <SndmanApp/>*/}
+                <SDIOutApp/>
                 {/* <AdminGuest/>*/}
                 {/*<AdminCongress/>*/}
-                {/*  <MobileClient/>*/}
                 {/* <AdminStreaming/>*/}
-                {/* <GalaxyStream/>*/}
-                {/* <VirtualStreaming/>*/}
             </Fragment>
         );
     }
