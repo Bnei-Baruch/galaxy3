e62731652ee7f46c1f77b19cebe7ea73336ad9c6
amnonbb <amnonbb@gmail.com>
2019-11-25 17:25:27 +0200

    GalaxyStream: set max width

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import GalaxyApp from "./apps/GalaxyApp";
+// import GalaxyApp from "./apps/GalaxyApp";
 // import OldClient from "./apps/VirtualApp/OldClient";
 // import MobileClient from "./apps/MobileApp/MobileClient";
 // import VirtualClient from "./apps/VirtualApp/VirtualClient";
 // import VirtualStreaming from "./apps/VirtualApp/VirtualStreaming";
 // import GroupClient from "./apps/GroupsApp/GroupClient";
-// import GalaxyStream from "./apps/StreamApp/GalaxyStream";
+import GalaxyStream from "./apps/StreamApp/GalaxyStream";
 // import ShidurAdmin from "./apps/ShidurApp/ShidurAdmin";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
 // import SndmanApp from "./apps/SndmanApp/SndmanApp";
@@ -20,13 +20,13 @@ class App extends Component {
     render() {
         return (
             <Fragment>
-                <GalaxyApp />
+                {/*<GalaxyApp />*/}
                 {/*<OldClient />*/}
                 {/*  <MobileClient/>*/}
                 {/*<VirtualClient/>*/}
                 {/* <VirtualStreaming/>*/}
                 {/*<GroupClient/>*/}
-                {/* <GalaxyStream/>*/}
+                 <GalaxyStream/>
                 {/* <ShidurAdmin/>*/}
                 {/* <ShidurApp/>*/}
                  {/*<SndmanApp/>*/}
