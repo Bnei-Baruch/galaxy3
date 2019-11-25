090befec944fa23728be42f9c4a19c32c02e779f
amnonbb <amnonbb@gmail.com>
2019-11-25 17:31:35 +0200

    GroupClient: open stream in new tab

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import 'semantic-ui-css/semantic.min.css';
 // import MobileClient from "./apps/MobileApp/MobileClient";
 // import VirtualClient from "./apps/VirtualApp/VirtualClient";
 // import VirtualStreaming from "./apps/VirtualApp/VirtualStreaming";
-// import GroupClient from "./apps/GroupsApp/GroupClient";
-import GalaxyStream from "./apps/StreamApp/GalaxyStream";
+import GroupClient from "./apps/GroupsApp/GroupClient";
+// import GalaxyStream from "./apps/StreamApp/GalaxyStream";
 // import ShidurAdmin from "./apps/ShidurApp/ShidurAdmin";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
 // import SndmanApp from "./apps/SndmanApp/SndmanApp";
@@ -25,8 +25,8 @@ class App extends Component {
                 {/*  <MobileClient/>*/}
                 {/*<VirtualClient/>*/}
                 {/* <VirtualStreaming/>*/}
-                {/*<GroupClient/>*/}
-                 <GalaxyStream/>
+                <GroupClient/>
+                {/* <GalaxyStream/>*/}
                 {/* <ShidurAdmin/>*/}
                 {/* <ShidurApp/>*/}
                  {/*<SndmanApp/>*/}
