d98bc96e39cc6619961fe2880c7cf25029030868
amnonbb <amnonbb@gmail.com>
2020-03-18 16:22:49 +0200

    SndmanApp: disable forward for virtual

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import 'semantic-ui-css/semantic.min.css';
 // import AdminShidur from "./apps/AdminApp/AdminShidur";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
 // import UsersApp from "./apps/ShidurApp/UsersApp";
-// import SndmanApp from "./apps/SndmanApp/SndmanApp";
-import AudioOutApp from "./apps/AudioOutApp/AudioOutApp";
+import SndmanApp from "./apps/SndmanApp/SndmanApp";
+// import AudioOutApp from "./apps/AudioOutApp/AudioOutApp";
 // import SDIOutApp from "./apps/SDIOutApp/SDIOutApp";
 // import UsersSDIOut from "./apps/SDIOutApp/UsersSDIOut";
 // import AdminGuest from "./apps/AdminApp/AdminGuest";
@@ -35,8 +35,8 @@ class App extends Component {
                 {/*<AdminShidur />*/}
                 {/* <ShidurApp/>*/}
                  {/*<UsersApp />*/}
-                 <AudioOutApp />
-                 {/*<SndmanApp/>*/}
+                 {/*<AudioOutApp />*/}
+                 <SndmanApp/>
                 {/*<SDIOutApp />*/}
                 {/*<UsersSDIOut />*/}
                 {/* <AdminGuest/>*/}
