e96dfba3f47488d59e577f2134726819be32c0bd
bbfsdev <bbfsdev@gmail.com>
2020-04-01 00:35:00 +0000

    Monitoring, fetch browser data. Send to server. Show local link info (only with deb param set).

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
+//import { isMobile } from 'react-device-detect';
 
 import './i18n/i18n';
 // import GalaxyApp from "./apps/GalaxyApp";
@@ -26,21 +27,22 @@ class App extends Component {
     return (
       <I18nextProvider i18n={i18n}>
         {/*<GalaxyApp />*/}
-        <OldClient />
+        {/*isMobile ? <MobileClient /> : <OldClient />*/}
+        {<OldClient />}
         {/*<MobileClient/>*/}
         {/*<VirtualClient />*/}
-        {/* <VirtualStreaming/>*/}
-        {/*<GroupClient/>*/}
-        {/* <GalaxyStream/>*/}
+        {/*<VirtualStreaming />*/}
+        {/*<GroupClient />*/}
+        {/*<GalaxyStream />*/}
         {/*<AdminRoot />*/}
         {/*<AdminShidur />*/}
-        {/* <AdminGuest/>*/}
-        {/*<ShidurApp/>*/}
-        {/*<SndmanApp/>*/}
+        {/*<AdminGuest />*/}
+        {/*<ShidurApp />*/}
+        {/*<SndmanApp />*/}
         {/*<AudioOutApp />*/}
         {/*<SDIOutApp />*/}
-        {/*<AdminCongress/>*/}
-        {/*<AdminStreaming/>*/}
+        {/*<AdminCongress />*/}
+        {/*<AdminStreaming />*/}
       </I18nextProvider>
     );
   }
