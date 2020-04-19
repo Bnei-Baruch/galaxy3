a2c9bca568ca3aa8532bd4cd0c10fa8b7d4023f7
Gregory Shilin <gshilin@gmail.com>
2020-04-19 20:33:28 +0300

    LoginMessage now conforms to its picture

--- src/components/LoginMessage.js
+++ src/components/LoginMessage.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react'
-import {Button, Container, Message, Icon} from 'semantic-ui-react'
-import {withTranslation} from "react-i18next";
+import React, { useState } from 'react';
+import { Button, Container, Grid, Icon, Message } from 'semantic-ui-react';
+import { useTranslation } from 'react-i18next';
 
-class LoginMessage extends Component {
-    state = { visible: true }
+const LoginMessage = () => {
+  const [visible, setVisible] = useState(true);
+  const { t }                 = useTranslation();
 
-    handleDismiss = () => {
-        this.setState({ visible: false })
-    }
+  let login = (<Button primary size='massive' onClick={() => window.open('https://galaxy.kli.one/user', '_self')}>LOGIN</Button>);
 
-    render() {
-        const { t } = this.props;
-        let login = (<Button primary size='massive' onClick={() => window.open("https://galaxy.kli.one/user","_self")} >LOGIN</Button>);
+  return visible
+    ? (
+      <Container textAlign='center' className="login-message">
+        <Message icon visible negative size='huge' onDismiss={() => setVisible(false)}>
+          <Message.Content>
+            <Grid celled>
+              <Grid.Row reversed>
+                <Grid.Column width={13} tablet={12} mobile={10}>
+                  {t('loginMessage.loginWarning1')} <a href='#' onClick={() => window.open('https://galaxy.kli.one/user', '_self')}>{t('loginMessage.register')}</a> {t('loginMessage.loginWarning2')}
+                </Grid.Column>
+                <Grid.Column width={3} textAlign='left'>
+                  <Icon>{login}</Icon>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Message.Content>
+        </Message>
+      </Container>
+    )
+    : null;
+};
 
-        if (this.state.visible) {
-            return (
-                <Container textAlign='center' >
-                    <Message icon visible negative size='huge' onDismiss={this.handleDismiss} >
-                        <Icon>{login}</Icon>
-                        <Message.Header>{t('loginMessage.loginWarning1')} <a href='#' onClick={() => window.open("https://galaxy.kli.one/user","_self")}>{t('loginMessage.register')}</a> {t('loginMessage.loginWarning2')}</Message.Header>
-                    </Message>
-                </Container>
-            )
-        }
-
-        return (
-            ""
-        )
-    }
-}
-
-export default withTranslation()(LoginMessage);
+export default LoginMessage;
