3381cf765541d64b112ecddb0ebe8e3405f104cd
amnonbb <amnonbb@gmail.com>
2020-10-07 22:41:19 +0300

    AdminRoot: get sessions from monitor server

--- src/apps/AdminApp/components/StatNotes.js
+++ src/apps/AdminApp/components/StatNotes.js
@@ -1,16 +1,6 @@
 import React, {Component} from "react";
 import {Label, Table} from "semantic-ui-react";
-import {genUUID} from "../../../shared/tools";
-import {
-    ADMIN_SECRET,
-    ADMIN_SRV_STR1,
-    ADMIN_SRV_STR2,
-    ADMIN_SRV_STR3,
-    ADMIN_SRV_STR4,
-    ADMIN_SRV_STR5,
-    ADMIN_SRV_STR6,
-    ADMIN_SRV_STR7
-} from "../../../shared/env";
+import {MONITOR_SRV} from "../../../shared/env";
 
 
 class StatNotes extends Component {
@@ -38,42 +28,26 @@ class StatNotes extends Component {
     };
 
     getCounts = () => {
-        this.getStrCounts();
-        const {data} = this.props;
-        const gxy1_count = data.filter(r => r.janus === "gxy1").map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-        const gxy2_count = data.filter(r => r.janus === "gxy2").map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-        const gxy3_count = data.filter(r => r.janus === "gxy3").map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-        const gxy4_count = data.filter(r => r.janus === "gxy4").map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-        const gxy5_count = data.filter(r => r.janus === "gxy5").map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-        const gxy6_count = data.filter(r => r.janus === "gxy6").map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-        const gxy7_count = data.filter(r => r.janus === "gxy7").map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-        const gxy8_count = data.filter(r => r.janus === "gxy8").map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-        this.setState({gxy1_count, gxy2_count, gxy3_count, gxy4_count, gxy5_count, gxy6_count, gxy7_count, gxy8_count});
+        fetch(`${MONITOR_SRV}`)
+            .then((response) => {
+            if (response.ok) {
+                return response.json().then(res => {
+                    const s = res.data.result;
+                    let state = {};
+                    for(let i=0; i<s.length; i++) {
+                        let key = s[i].metric.name;
+                        let val = parseInt(s[i].value[1], 10);
+                        state = {...state, [key+"_count"]: val}
+                    }
+                    this.setState(state);
+                });
+            }
+        })
     };
 
-    getStrCounts = () => {
-        const servers = [ADMIN_SRV_STR1,ADMIN_SRV_STR2,ADMIN_SRV_STR3,ADMIN_SRV_STR4,ADMIN_SRV_STR5,ADMIN_SRV_STR6,ADMIN_SRV_STR7];
-        for(let i=0; i<servers.length; i++) {
-            let request = {"janus":"list_sessions","transaction": genUUID(),"admin_secret": ADMIN_SECRET};
-            fetch(servers[i],{
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body:  JSON.stringify(request)
-            }).then((response) => {
-                if (response.ok) {
-                    return response.json().then(data => {
-                        this.setState({["str"+(i+1)+"_count"]: data.sessions.length})
-                    });
-                }
-            })
-        }
-    }
-
     render() {
-        const {} = this.props;
         const {gxy1_count, gxy2_count, gxy3_count, gxy4_count, gxy5_count, gxy6_count, gxy7_count, gxy8_count,
             str1_count, str2_count, str3_count, str4_count, str5_count, str6_count, str7_count} = this.state;
-
 
         return (
             <Label attached='top right' size='mini' className='gxy_count' >
