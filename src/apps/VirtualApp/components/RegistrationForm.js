47ad29ce7d1a3e377c33c9808df375a884b2d01c
davgur <davgur.ru@gmail.com>
2020-11-15 15:45:49 +0200

    add in progress to button

--- src/apps/VirtualApp/components/RegistrationForm.js
+++ src/apps/VirtualApp/components/RegistrationForm.js
@@ -13,6 +13,7 @@ import {REGISTRATION_FORM_FIELDS} from '../../../shared/env';
 import api from '../../../shared/Api';
 import countries from "i18n-iso-countries";
 import {SelectViewLanguage} from "./SelectViewLanguage";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles(() => ({
   container: {
@@ -38,301 +39,310 @@ const fetchCountriesByLang = (lang = 'en') => {
 }
 
 export const RegistrationForm = ({user: {familyname, username, email, display}, id, onClose, onSubmit, isOpen, language}) => {
-    const classes = useStyles();
-    const [tens, setTens] = useState();
-    const [city, setCity] = useState();
-    const [country, setCountry] = useState();
-    const [gender, setGender] = useState();
-    const [telephone, setTelephone] = useState();
-    const [aboutYou, setAboutYou] = useState();
-    const [ten, setTen] = useState();
-    const [errors, setErrors] = useState({});
+  const classes = useStyles();
+  const [tens, setTens] = useState();
+  const [city, setCity] = useState();
+  const [country, setCountry] = useState();
+  const [gender, setGender] = useState();
+  const [telephone, setTelephone] = useState();
+  const [aboutYou, setAboutYou] = useState();
+  const [ten, setTen] = useState();
+  const [errors, setErrors] = useState({});
+  const [isProgress, setIsProgress] = useState(false);
 
-    const [userLanguage, setUserLanguage] = useState();
-    const {t} = useTranslation();
-    const direction = language === 'he' ? 'rtl' : '';
+  const [userLanguage, setUserLanguage] = useState();
+  const {t} = useTranslation();
+  const direction = language === 'he' ? 'rtl' : '';
 
 
   useEffect(() => {
-      fetchRooms();
-    }, []);
+    fetchRooms();
+  }, []);
 
-    useEffect(() => {
-      fetchCountriesByLang(language);
-    }, [language]);
+  useEffect(() => {
+    fetchCountriesByLang(language);
+  }, [language]);
 
   const fetchRooms = () => {
     api.fetchAvailableRooms()
       .then(d => {
         const _tens = d.rooms.map(r => r.description).filter(t => !t.match(/test|info/i));
         setTens(_tens);
-      }).catch(()=>setTens([]));
+      }).catch(() => setTens([]));
   };
 
-    const handleCityChange = ({target: {value}}) => {
-      deleteErrorByKey('city');
-      setCity(value);
-    };
-    const handleGenderChange = ({target: {value}}) => {
-      deleteErrorByKey('gender')
-      setGender(value);
-    }
-    const handleTelephoneChange = ({target: {value}}) => {
-      deleteErrorByKey('telephone')
-      setTelephone(value)
-    };
-    const handleAboutYouChange = ({target: {value}}) => setAboutYou(value);
-    const handleTenChange = (e, op) => {
-      if (!op)
-        return;
-      setTen(op);
-    };
-    const handleCountryChange = (e, op) => {
-      deleteErrorByKey('country');
-      if (!op)
-        return;
-      setCountry(op);
-    };
+  const handleCityChange = ({target: {value}}) => {
+    deleteErrorByKey('city');
+    setCity(value);
+  };
+  const handleGenderChange = ({target: {value}}) => {
+    deleteErrorByKey('gender')
+    setGender(value);
+  }
+  const handleTelephoneChange = ({target: {value}}) => {
+    deleteErrorByKey('telephone')
+    setTelephone(value)
+  };
+  const handleAboutYouChange = ({target: {value}}) => setAboutYou(value);
+  const handleTenChange = (e, op) => {
+    if (!op)
+      return;
+    setTen(op);
+  };
+  const handleCountryChange = (e, op) => {
+    deleteErrorByKey('country');
+    if (!op)
+      return;
+    setCountry(op);
+  };
 
-    const handleLanguageChange = (e, op) => {
-      deleteErrorByKey('language');
-      if (!op)
-        return;
-      setUserLanguage(op);
-    };
+  const handleLanguageChange = (e, op) => {
+    deleteErrorByKey('language');
+    if (!op)
+      return;
+    setUserLanguage(op);
+  };
 
-    const deleteErrorByKey = (key) => {
-      if (!errors[key])
-        return;
-      const newErr = {...errors}
-      delete newErr[key];
-      setErrors(newErr)
-    }
+  const deleteErrorByKey = (key) => {
+    if (!errors[key])
+      return;
+    const newErr = {...errors}
+    delete newErr[key];
+    setErrors(newErr)
+  }
 
 
-    const renderTens = () => {
-      return tens && (
-        <Autocomplete
-          variant="outlined"
-          value={ten}
-          options={tens}
-          getOptionLabel={r => r}
-          onChange={handleTenChange}
-          renderInput={
-            (params) => (
-              <TextField {...params} label={t('registration.selectGroup')} variant="outlined"/>
-            )
-          }
-        />
-      );
-    };
-
-    const renderCountries = () => {
-      return countryIds && (
-        <Autocomplete
-          variant="outlined"
-          value={country}
-          options={countryIds}
-          getOptionLabel={id => countryById[id]}
-          onChange={handleCountryChange}
-          renderInput={
-            (params) => (
-              <TextField
-                {...params}
-                label={t('registration.selectCountry')}
-                variant="outlined"
-                required
-                error={errors.country}
-              />
-            )
-          }
-        />
-      );
-    };
-
-    const renderLanguages = () => {
-      return LANGUAGES && (
-        <Autocomplete
-          variant="outlined"
-          value={userLanguage}
-          options={LANGUAGES}
-          getOptionLabel={r => r.nativeName}
-          onChange={handleLanguageChange}
-          renderInput={
-            (params) => (
-              <TextField
-                {...params}
-                label={t('registration.selectLanguage')}
-                variant="outlined"
-                required
-                error={errors.language}
-              />
-            )
-          }
-        />
-      );
-    };
-
-    const renderGender = () => (
-      <TextField
-        select
-        fullWidth={true}
+  const renderTens = () => {
+    return tens && (
+      <Autocomplete
         variant="outlined"
-        label={t('registration.selectGender')}
-        onChange={handleGenderChange}
-        value={gender}
-        required
-        error={errors.gender}
-      >
-        <MenuItem key="male" value="male">
-          {t('registration.male')}
-        </MenuItem>
-        <MenuItem key="female" value="female">
-          {t('registration.female')}
-        </MenuItem>
-      </TextField>
+        value={ten}
+        options={tens}
+        getOptionLabel={r => r}
+        onChange={handleTenChange}
+        renderInput={
+          (params) => (
+            <TextField {...params} label={t('registration.selectGroup')} variant="outlined"/>
+          )
+        }
+      />
     );
+  };
 
-    const submit = async () => {
-      if (!validateForm())
-        return;
-
-      let hasError = false;
-      try {
-        const formOpt = {
-          method: 'POST',
-          body: buildRequestBody(),
-          mode: 'no-cors'
-        };
-        await fetch('https://docs.google.com/forms/u/0/d/e/1FAIpQLSdQZ1TZ2WihdhyuWrT7mVT8AoTHfX4dNjYtEoB5HBFP_9HttA/formResponse', formOpt);
-      } catch (e) {
-        (e.status === 403) && (hasError = true);
-      }
-
-      if (hasError)
-        return onSubmit();
-
-      try {
-        await updateKCStatus();
-      } catch (e) {
-        console.log('change kc status error', e);
-      }
-      onSubmit();
-    };
-
-    const validateForm = () => {
-      let e = {};
-      if (!city) e.city = true;
-      if (!country) e.country = true;
-      if (!userLanguage) e.language = true;
-      if (!telephone) e.telephone = true;
-      if (!gender) e.gender = true;
-      setErrors(e);
-      return Object.keys(e).length === 0;
-    };
-
-    const buildRequestBody = () => {
-      const body = new FormData();
-      console.log('buildRequestBody', countries.getName(country, "en", {select: "official"}))
-      body.set(REGISTRATION_FORM_FIELDS.country, countries.getName(country, "en", {select: "official"}));
-      body.set(REGISTRATION_FORM_FIELDS.city, city);
-      body.set(REGISTRATION_FORM_FIELDS.aboutYou, aboutYou);
-      body.set(REGISTRATION_FORM_FIELDS.gender, gender);
-      body.set(REGISTRATION_FORM_FIELDS.ten, ten);
-      body.set(REGISTRATION_FORM_FIELDS.telephone, telephone);
-      body.set(REGISTRATION_FORM_FIELDS.language, userLanguage.name);
-      body.set(REGISTRATION_FORM_FIELDS.userId, id);
-      body.set(REGISTRATION_FORM_FIELDS.firstName, username);
-      body.set(REGISTRATION_FORM_FIELDS.familyName, familyname);
-      body.set(REGISTRATION_FORM_FIELDS.email, email);
-      debugger
-      return body;
-    };
-
-    const updateKCStatus = async () => {
-      const opt = api.defaultOptions();
-      opt.method = 'POST';
-      return await fetch('https://acc.kli.one/api/pending', opt);
-    };
-
-    const renderForm = () => (
-      <>
-        <Typography variant="h4" display={'block'} paragraph>
-          {t('registration.welcome', {name: display})}
-        </Typography>
-        <Typography paragraph>
-          {t('registration.needVerifyAccount')}
-        </Typography>
-        <Typography paragraph>
-          {t('registration.tellAboutYou')}
-        </Typography>
-
-        <Grid container spacing={6}>
-          <Grid item xs={6}>
-            {renderCountries()}
-          </Grid>
-          <Grid item xs={6}>
+  const renderCountries = () => {
+    return countryIds && (
+      <Autocomplete
+        variant="outlined"
+        value={country}
+        options={countryIds}
+        getOptionLabel={id => countryById[id]}
+        onChange={handleCountryChange}
+        renderInput={
+          (params) => (
             <TextField
-              label={t('registration.groupCity')}
+              {...params}
+              label={t('registration.selectCountry')}
               variant="outlined"
-              onChange={handleCityChange}
-              value={city}
-              fullWidth
               required
-              error={errors.city}
+              error={errors.country}
             />
-          </Grid>
-          <Grid item xs={6}>
-            {renderGender()}
-          </Grid>
-          <Grid item xs={6}>
-            {renderLanguages()}
-          </Grid>
-          <Grid item xs={6}>
+          )
+        }
+      />
+    );
+  };
+
+  const renderLanguages = () => {
+    return LANGUAGES && (
+      <Autocomplete
+        variant="outlined"
+        value={userLanguage}
+        options={LANGUAGES}
+        getOptionLabel={r => r.nativeName}
+        onChange={handleLanguageChange}
+        renderInput={
+          (params) => (
             <TextField
-              label={t('registration.telephone')}
+              {...params}
+              label={t('registration.selectLanguage')}
               variant="outlined"
-              onChange={handleTelephoneChange}
-              value={telephone}
-              fullWidth
               required
-              error={errors.telephone}
+              error={errors.language}
             />
-          </Grid>
-          <Grid item xs={6}>
-            {renderTens()}
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label={t('registration.aboutYou')}
-              variant="outlined"
-              onChange={handleAboutYouChange}
-              value={aboutYou}
-              multiline
-              fullWidth
-            />
-          </Grid>
-          <Grid container justify="center">
-            <Button
-              variant="contained"
-              onClick={submit}
-              className={classes.button}
-            >
-              {t('registration.submit')}
-            </Button>
-          </Grid>
+          )
+        }
+      />
+    );
+  };
+
+  const renderGender = () => (
+    <TextField
+      select
+      fullWidth={true}
+      variant="outlined"
+      label={t('registration.selectGender')}
+      onChange={handleGenderChange}
+      value={gender}
+      required
+      error={errors.gender}
+    >
+      <MenuItem key="male" value="male">
+        {t('registration.male')}
+      </MenuItem>
+      <MenuItem key="female" value="female">
+        {t('registration.female')}
+      </MenuItem>
+    </TextField>
+  );
+
+  const submit = async () => {
+    if (!validateForm())
+      return;
+
+    setIsProgress(true);
+    let hasError = false;
+    try {
+      const formOpt = {
+        method: 'POST',
+        body: buildRequestBody(),
+        mode: 'no-cors'
+      };
+      await fetch('https://docs.google.com/forms/u/0/d/e/1FAIpQLSdQZ1TZ2WihdhyuWrT7mVT8AoTHfX4dNjYtEoB5HBFP_9HttA/formResponse', formOpt);
+    } catch (e) {
+      (e.status === 403) && (hasError = true);
+    }
+
+    if (hasError) {
+      setIsProgress(false);
+      return onSubmit();
+    }
+
+    try {
+      await updateKCStatus();
+    } catch (e) {
+      console.log('change kc status error', e);
+    }
+    setIsProgress(false);
+    onSubmit();
+  };
+
+  const validateForm = () => {
+    let e = {};
+    if (!city) e.city = true;
+    if (!country) e.country = true;
+    if (!userLanguage) e.language = true;
+    if (!telephone) e.telephone = true;
+    if (!gender) e.gender = true;
+    setErrors(e);
+    return Object.keys(e).length === 0;
+  };
+
+  const buildRequestBody = () => {
+    const body = new FormData();
+    console.log('buildRequestBody', countries.getName(country, "en", {select: "official"}))
+    body.set(REGISTRATION_FORM_FIELDS.country, countries.getName(country, "en", {select: "official"}));
+    body.set(REGISTRATION_FORM_FIELDS.city, city);
+    body.set(REGISTRATION_FORM_FIELDS.aboutYou, aboutYou);
+    body.set(REGISTRATION_FORM_FIELDS.gender, gender);
+    body.set(REGISTRATION_FORM_FIELDS.ten, ten);
+    body.set(REGISTRATION_FORM_FIELDS.telephone, telephone);
+    body.set(REGISTRATION_FORM_FIELDS.language, userLanguage.name);
+    body.set(REGISTRATION_FORM_FIELDS.userId, id);
+    body.set(REGISTRATION_FORM_FIELDS.firstName, username);
+    body.set(REGISTRATION_FORM_FIELDS.familyName, familyname);
+    body.set(REGISTRATION_FORM_FIELDS.email, email);
+    debugger
+    return body;
+  };
+
+  const updateKCStatus = async () => {
+    const opt = api.defaultOptions();
+    opt.method = 'POST';
+    return await fetch('https://acc.kli.one/api/pending', opt);
+  };
+
+  const renderForm = () => (
+    <>
+      <Typography variant="h4" display={'block'} paragraph>
+        {t('registration.welcome', {name: display})}
+      </Typography>
+      <Typography paragraph>
+        {t('registration.needVerifyAccount')}
+      </Typography>
+      <Typography paragraph>
+        {t('registration.tellAboutYou')}
+      </Typography>
+
+      <Grid container spacing={6}>
+        <Grid item xs={6}>
+          {renderCountries()}
         </Grid>
-      </>
-    );
+        <Grid item xs={6}>
+          <TextField
+            label={t('registration.groupCity')}
+            variant="outlined"
+            onChange={handleCityChange}
+            value={city}
+            fullWidth
+            required
+            error={errors.city}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          {renderGender()}
+        </Grid>
+        <Grid item xs={6}>
+          {renderLanguages()}
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            label={t('registration.telephone')}
+            variant="outlined"
+            onChange={handleTelephoneChange}
+            value={telephone}
+            fullWidth
+            required
+            error={errors.telephone}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          {renderTens()}
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label={t('registration.aboutYou')}
+            variant="outlined"
+            onChange={handleAboutYouChange}
+            value={aboutYou}
+            multiline
+            fullWidth
+          />
+        </Grid>
+        <Grid container justify="center">
+          <Button
+            variant="contained"
+            onClick={submit}
+            className={classes.button}
+            disabled={isProgress}
+          >
+            {t('registration.submit')}
+            {
+              isProgress ? <CircularProgress/> : null
+            }
+          </Button>
+        </Grid>
+      </Grid>
+    </>
+  );
 
-    return (
-      <Modal
-        open={isOpen}
-        onClose={onClose}
-        disableBackdropClick
-        style={{verticalAlign: 'middle'}}
-        dir={direction}
-      >
+  return (
+    <Modal
+      open={isOpen}
+      onClose={onClose}
+      disableBackdropClick
+      style={{verticalAlign: 'middle'}}
+      dir={direction}
+    >
 
       <Box
         className={classes.container}
