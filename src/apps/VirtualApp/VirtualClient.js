bbb2dd2a3374e1572bfaf139f329aec7c0c89102
davgur <davgur.ru@gmail.com>
2020-12-14 18:10:18 +0200

    artefact

--- src/apps/VirtualApp/VirtualClient.js
+++ src/apps/VirtualApp/VirtualClient.js
@@ -93,8 +93,8 @@ const userFeeds = (feeds) => feeds.filter(feed => feed.display.role === userRole
 
 //for test server
 // const isUseNewDesign = new URL(window.location.href).searchParams.has('new_design');
-const isUseNewDesign = true;
-//const isUseNewDesign = window.location.hostname === 'arvut.kli.one';
+//const isUseNewDesign = true;
+const isUseNewDesign = window.location.hostname === 'arvut.kli.one';
 
 class VirtualClient extends Component {
 
