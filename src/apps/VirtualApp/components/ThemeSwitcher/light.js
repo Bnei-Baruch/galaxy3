f58a20046da3722a267f59fda22c2be5fe3fa1d7
davgur <davgur.ru@gmail.com>
2020-12-14 12:54:28 +0200

    use background from theme

--- src/apps/VirtualApp/components/ThemeSwitcher/light.js
+++ src/apps/VirtualApp/components/ThemeSwitcher/light.js
@@ -2,24 +2,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
 const light = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#cc4444',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#f5f5f5',
-    },
-    titleBar: {
-      main: '#eeeeee',
-      contrastText: '#222222',
-    },
-  },
+  palette: {},
 });
 
 export default light;
