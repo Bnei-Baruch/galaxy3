57e4beaf848e3ee1846bd1446b89fdfa4272f239
davgur <davgur.ru@gmail.com>
2021-02-08 10:41:23 +0200

    init crisp from virtual client

--- src/apps/VirtualApp/components/Support/helper.js
+++ src/apps/VirtualApp/components/Support/helper.js
@@ -1,42 +1,48 @@
-export const openCrisp = () => {
-  if (window.$crisp) {
-    window.$crisp.push(['do', 'chat:show']);
-    window.$crisp.push(['do', 'chat:open']);
-  }
+let isInit              = false;
+
+export const closeCrisp = () => {
+  if (!window.$crisp) return;
+  window.$crisp.push(['do', 'chat:hide']);
 };
 
-export const initCrisp = (locale, uiLocale, user, callback) => {
-  if (window.$crisp) {
-    const script = document.querySelector('#crisp');
-    if (script) {
-      script.remove();
-    }
-    window.$crisp = undefined;
-  }
-  if (!window.$crisp) {
-    window.$crisp = [
-      ['do', 'chat:hide'],
-      ["on", "chat:closed", () => window.$crisp.push(['do', 'chat:hide'])],
-      ["on", "message:received", openCrisp],
-    ];
-    if (user) {
-      window.$crisp.push(["set", "user:email", [user.email]]);
-      window.$crisp.push(["set", "user:nickname", [user.display]]);
-    }
-    if (callback) {
-      window.$crisp.push(['on', 'session:loaded', callback]);
-    }
-    window.CRISP_WEBSITE_ID = "a88f7eac-d881-450b-b589-ab82160fb08a";
-    window.CRISP_RUNTIME_CONFIG = {locale: locale || uiLocale};
+export const openCrisp = () => {
+  if (!window.$crisp) return;
+  window.$crisp.push(['do', 'chat:open']);
+  window.$crisp.push(['do', 'chat:show']);
+};
 
-    (() => {
-      var d = document;
-      var s = d.createElement("script");
-      s.id = 'crisp';
+export const initCrisp = () => {
+  if (isInit) return;
+  isInit = true;
 
-      s.src = "https://client.crisp.chat/l.js";
-      s.async = 1;
-      d.getElementsByTagName("head")[0].appendChild(s);
-    })();
+  window.CRISP_WEBSITE_ID    = 'a88f7eac-d881-450b-b589-ab82160fb08a';
+  window.CRISP_READY_TRIGGER = function () {
+    window.$crisp.push(['do', 'chat:hide']);
+    window.$crisp.push(['do', 'chat:close']);
+  };
+
+  (() => {
+    var d = document;
+    var s = d.createElement('script');
+    s.id  = 'crisp';
+
+    s.src   = 'https://client.crisp.chat/l.js';
+    s.async = 1;
+    d.getElementsByTagName('head')[0].appendChild(s);
+  })();
+};
+
+export const configCrisp = (locale, uiLocale, user) => {
+  if (!window.$crisp) return;
+
+  if (user) {
+    window.$crisp.push(['set', 'user:email', [user.email]]);
+    window.$crisp.push(['set', 'user:nickname', [user.display]]);
   }
+
+  window.$crisp.push(['on', 'chat:closed', closeCrisp]);
+  window.$crisp.push(['on', 'message:received', openCrisp]);
+
+  window.CRISP_RUNTIME_CONFIG = { locale: locale || uiLocale };
+  window.$crisp.push(['do', 'session:reset', [false]]);
 };
