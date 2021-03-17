95ee6e9f03e5542cb1e0e988ad0308812497c717
amnonbb <amnonbb@gmail.com>
2021-03-17 19:02:02 +0200

    WebOutApp: clean

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import i18n from 'i18next';
 import './i18n/i18n';
 // import GalaxyApp from "./apps/GalaxyApp";
 // import MobileClient from "./apps/MobileApp/MobileClient";
-import VirtualClient from './apps/VirtualApp/VirtualClient';
+// import VirtualClient from './apps/VirtualApp/VirtualClient';
 // import GalaxyStream from "./apps/StreamApp/GalaxyStream";
 // import AdminRoot from "./apps/AdminApp/AdminRoot";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
@@ -14,7 +14,7 @@ import VirtualClient from './apps/VirtualApp/VirtualClient';
 // import AudioOutApp from "./apps/AudioOutApp/AudioOutApp";
 // import AdminApp from "./apps/AdminApp/AdminApp";
 // import SDIOutApp from "./apps/SDIOutApp/SDIOutApp";
-// import WebOutApp from "./apps/WebOutApp/WebOutApp";
+import WebOutApp from "./apps/WebOutApp/WebOutApp";
 
 class App extends Component {
   render() {
@@ -22,7 +22,7 @@ class App extends Component {
       <I18nextProvider i18n={i18n}>
         {/*{<GalaxyApp />}*/}
         {/*<MobileClient />*/}
-        <VirtualClient />
+        {/*<VirtualClient />*/}
         {/*<GalaxyStream/>*/}
         {/*{<AdminRoot />}*/}
         {/*<AdminApp />*/}
@@ -30,7 +30,7 @@ class App extends Component {
         {/*<SndmanApp />*/}
         {/*<AudioOutApp />*/}
         {/*<SDIOutApp />*/}
-        {/*<WebOutApp />*/}
+        <WebOutApp />
       </I18nextProvider>
     );
   }
--- src/apps/WebOutApp/WebOutApp.js
+++ src/apps/WebOutApp/WebOutApp.js
@@ -2,13 +2,11 @@ import React, {Component, Fragment} from 'react';
 import {Grid} from "semantic-ui-react";
 import './WebOutApp.css';
 import './UsersQuad.scss'
-import {USERNAME_ALREADY_EXIST_ERROR_CODE, SDIOUT_ID} from "../../shared/consts";
 import api from "../../shared/Api";
 import {API_BACKEND_PASSWORD, API_BACKEND_USERNAME} from "../../shared/env";
 import GxyJanus from "../../shared/janus-utils";
 import UsersQuad from "./UsersQuad";
-import {GuaranteeDeliveryManager} from '../../shared/GuaranteeDelivery';
-import {captureException, captureMessage} from "../../shared/sentry";
+import {captureException} from "../../shared/sentry";
 
 
 class WebOutApp extends Component {
@@ -37,7 +35,6 @@ class WebOutApp extends Component {
     gatewaysInitialized: false,
     appInitError: null,
     vote: false,
-    gdm: new GuaranteeDeliveryManager(SDIOUT_ID),
     roomsStatistics: {},
     reinit_inst: null,
     pnum: {},
@@ -57,11 +54,9 @@ class WebOutApp extends Component {
   };
 
   fetchRooms = () => {
-    let {disabled_rooms, groups, shidur_mode, preusers_count, region, region_groups} = this.state;
+    let {disabled_rooms, groups, shidur_mode, region_groups} = this.state;
     api.fetchActiveRooms()
       .then((data) => {
-        //const users_count = data.map(r => r.num_users).reduce((su, cur) => su + cur, 0);
-
         let rooms = data;
 
         if(shidur_mode === "nashim") {
@@ -73,14 +68,6 @@ class WebOutApp extends Component {
         }
 
         groups = rooms.filter(r => r.users.filter(r => r.camera).length > 3);
-        //groups = rooms.filter(r => !r.extra?.disabled);
-
-        // Extra exist and disabled
-        //disabled_rooms = rooms.filter(r => r.extra?.disabled);
-
-        //let quads = [...this.col1.state.vquad,...this.col2.state.vquad,...this.col3.state.vquad,...this.col4.state.vquad];
-        //let list = groups.filter(r => !quads.find(q => q && r.room === q.room));
-        //let questions = list.filter(room => room.questions);
         this.setState({rooms, groups, disabled_rooms, region_groups});
       })
       .catch(err => {
@@ -106,104 +93,7 @@ class WebOutApp extends Component {
   initGateways = (user) => {
     const gateways = GxyJanus.makeGateways("rooms");
     this.setState({gateways});
-
-    const gatewayToInitPromise = (gateway) => this.initGateway(user, gateway)
-      .catch(error => {
-        captureException(error, {source: 'SDIOut', gateway: gateway.name});
-        throw error;
-      });
-
-    return Promise.all(Object.values(gateways).map(gatewayToInitPromise))
-      .then(() => {
-        console.log("[SDIOut] gateways initialization complete");
-        this.setState({gatewaysInitialized: true});
-      });
-  };
-
-  initGateway = (user, gateway) => {
-    console.log("[SDIOut] initializing gateway", gateway.name);
-
-    gateway.addEventListener("reinit", () => {
-        this.setState({reinit_inst: gateway.name});
-        this.postInitGateway(user, gateway)
-          .catch(err => {
-            console.error("[SDIOut] postInitGateway error after reinit. Reloading", gateway.name, err);
-            captureException(err, {source: 'SDIOut', gateway: gateway.name});
-            this.initGateway(user, gateway);
-          });
-      }
-    );
-
-    return gateway.init()
-      .then(() => this.postInitGateway(user, gateway))
-      .catch(err => {
-        console.error("[SDIOut] error initializing gateway", gateway.name, err);
-        setTimeout(() => {
-          this.initGateway(user, gateway)
-            .catch(err => captureException(err, {source: 'SDIOut', gateway: gateway.name}));
-        }, 10000);
-      });
-  };
-
-  postInitGateway = (user, gateway) => {
-    // if (gateway.name === "gxy3") {
-    //   return gateway.initServiceProtocol(user, data => this.onServiceData(gateway, data));
-    // }
-    return Promise.resolve();
-  };
-
-  onServiceData = (gateway, data, user) => {
-    const { gdm } = this.state;
-    if (gdm.checkAck(data)) {
-      // Ack received, do nothing.
-      return;
-    }
-    gdm.accept(data, (msg) => gateway.sendServiceMessage(msg)).then((data) => {
-      if (data === null) {
-        console.log('Message received more then once.');
-        return;
-      }
-
-      if (data.type === "error") {
-        if (data.error_code === USERNAME_ALREADY_EXIST_ERROR_CODE) {
-          console.error("[SDIOut] service error message (reloading in 10 seconds)", data.error);
-          captureMessage(data.error, {source: "SDIOut", msg: data});
-          setTimeout(() => {
-            this.initGateway(user, gateway);
-          }, 10000);
-        } else {
-          captureException(data.error, {source: "SDIOut", msg: data});
-        }
-      }
-
-      const {room, col, feed, group, i, status, qst} = data;
-
-      if(data.type === "sdi-fullscr_group" && status) {
-        if(qst) {
-          this.setState({col, i, group, room, qg: this.state.qids["q"+col].vquad[i]})
-        } else {
-          this["col"+col].toFullGroup(i,feed);
-        }
-      } else if(data.type === "sdi-fullscr_group" && !status) {
-        let {col, feed, i} = data;
-        if(qst) {
-          this.setState({group: null, room: null, qg: null});
-        } else {
-          this["col"+col].toFourGroup(i,feed);
-        }
-      } else if(data.type === "sdi-vote") {
-        if(this.state.group)
-          return
-        this.setState({vote: status, qg: null});
-      } else if(data.type === "sdi-restart_sdiout") {
-        window.location.reload();
-      } else if(data.type === "event") {
-        delete data.type;
-        this.setState({...data});
-      }
-    }).catch((error) => {
-      console.error(`Failed receiving ${data}: ${error}`);
-    });
+    Object.values(gateways).map(gateway => gateway.init());
   };
 
   setProps = (props) => {
@@ -211,20 +101,7 @@ class WebOutApp extends Component {
   };
 
   render() {
-    let {appInitError, gatewaysInitialized,qg} = this.state;
-    if (appInitError) {
-      return (
-        <Fragment>
-          <h1>Error Initializing Application</h1>
-          {`${appInitError}`}
-        </Fragment>
-      );
-    }
-
-    if (!gatewaysInitialized) {
-      return "Initializing WebRTC gateways...";
-    }
-
+    let {qg} = this.state;
     return (
       <Grid columns={2} className="sdi_container">
         <Grid.Row className="sdi_top">
