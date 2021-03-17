1735f9238ad704f37f7751f74a9c91a176271e49
amnonbb <amnonbb@gmail.com>
2021-03-17 18:19:53 +0200

    SDIOutApp: indention

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import i18n from 'i18next';
 import './i18n/i18n';
 // import GalaxyApp from "./apps/GalaxyApp";
 // import MobileClient from "./apps/MobileApp/MobileClient";
-import VirtualClient from './apps/VirtualApp/VirtualClient';
+// import VirtualClient from './apps/VirtualApp/VirtualClient';
 // import GalaxyStream from "./apps/StreamApp/GalaxyStream";
 // import AdminRoot from "./apps/AdminApp/AdminRoot";
 // import ShidurApp from "./apps/ShidurApp/ShidurApp";
 // import SndmanApp from "./apps/SndmanApp/SndmanApp";
 // import AudioOutApp from "./apps/AudioOutApp/AudioOutApp";
 // import AdminApp from "./apps/AdminApp/AdminApp";
-// import SDIOutApp from "./apps/SDIOutApp/SDIOutApp";
+import SDIOutApp from "./apps/SDIOutApp/SDIOutApp";
 // import WebOutApp from "./apps/WebOutApp/WebOutApp";
 
 class App extends Component {
@@ -22,14 +22,14 @@ class App extends Component {
       <I18nextProvider i18n={i18n}>
         {/*{<GalaxyApp />}*/}
         {/*<MobileClient />*/}
-        <VirtualClient />
+        {/*<VirtualClient />*/}
         {/*<GalaxyStream/>*/}
         {/*{<AdminRoot />}*/}
         {/*<AdminApp />*/}
         {/*<ShidurApp />*/}
         {/*<SndmanApp />*/}
         {/*<AudioOutApp />*/}
-        {/*<SDIOutApp />*/}
+        <SDIOutApp />
         {/*<WebOutApp />*/}
       </I18nextProvider>
     );
--- src/apps/SDIOutApp/SDIOutApp.js
+++ src/apps/SDIOutApp/SDIOutApp.js
@@ -2,149 +2,102 @@ import React, {Component, Fragment} from 'react';
 import {Grid, Segment} from "semantic-ui-react";
 import './SDIOutApp.css';
 import './UsersQuadSDIOut.scss'
-import {USERNAME_ALREADY_EXIST_ERROR_CODE, SDIOUT_ID} from "../../shared/consts";
+import {SDIOUT_ID} from "../../shared/consts";
 import api from "../../shared/Api";
 import {API_BACKEND_PASSWORD, API_BACKEND_USERNAME} from "../../shared/env";
 import GxyJanus from "../../shared/janus-utils";
 import UsersHandleSDIOut from "./UsersHandleSDIOut";
 import UsersQuadSDIOut from "./UsersQuadSDIOut";
-import {GuaranteeDeliveryManager} from '../../shared/GuaranteeDelivery';
-import {captureException, captureMessage} from "../../shared/sentry";
+import {captureException} from "../../shared/sentry";
 import mqtt from "../../shared/mqtt";
-import ConfigStore from "../../shared/ConfigStore";
 
 
 class SDIOutApp extends Component {
 
-    state = {
-        qg: null,
-        group: null,
-        room: null,
-        user: {
-            session: 0,
-            handle: 0,
-            role: "sdiout",
-            display: "sdiout",
-            id: SDIOUT_ID,
-            name: "sdiout",
-            email: "sdiout@galaxy.kli.one"
-        },
-        qids: [],
-        qcol: 0,
-        gateways: {},
-        gatewaysInitialized: false,
-        appInitError: null,
-        vote: false,
-        gdm: new GuaranteeDeliveryManager(SDIOUT_ID),
-        roomsStatistics: {},
-        reinit_inst: null,
-    };
+  state = {
+    qg: null,
+    group: null,
+    room: null,
+    user: {
+      session: 0,
+      handle: 0,
+      role: "sdiout",
+      display: "sdiout",
+      id: SDIOUT_ID,
+      name: "sdiout",
+      email: "sdiout@galaxy.kli.one"
+    },
+    qids: [],
+    qcol: 0,
+    gateways: {},
+    appInitError: null,
+    vote: false,
+    roomsStatistics: {},
+    reinit_inst: null,
+  };
 
-    componentDidMount() {
-        setInterval(() => {
-            api.fetchProgram()
-                .then(qids => {
-                    this.setState({qids});
-                    if(this.state.qg) {
-                        const {col, i} = this.state;
-                        this.setState({qg: this.state.qids["q"+col].vquad[i]})
-                    }
-                })
-                .catch(err => {
-                    console.error("[SDIOut] error fetching quad state", err);
-                    captureException(err, {source: "SDIOut"});
-                });
-
-            api.fetchRoomsStatistics()
-                .then((roomsStatistics) => {
-                    this.setState({roomsStatistics});
-                })
-                .catch(err => {
-                    console.error("[SDIOut] error fetching rooms statistics", err);
-                    captureException(err, {source: "SDIOut"});
-                });
-        }, 1000);
-        this.initApp();
-    };
-
-    componentWillUnmount() {
-        Object.values(this.state.gateways).forEach(x => x.destroy());
-    };
-
-    initApp = () => {
-        const {user} = this.state;
-
-        api.setBasicAuth(API_BACKEND_USERNAME, API_BACKEND_PASSWORD);
-
-        api.fetchConfig()
-            .then(data => GxyJanus.setGlobalConfig(data))
-            .then(() => this.initGateways(user))
-            .catch(err => {
-                console.error("[SDIOut] error initializing app", err);
-                this.setState({appInitError: err});
-                captureException(err, {source: 'SDIOut'});
-            });
-    };
-
-    initGateways = (user) => {
-        mqtt.init(user, (data) => {
-          console.log("[SDIOut] mqtt init: ", data)
-          setTimeout(() => {
-            mqtt.watch((data) => {
-              this.onMqttData(data);
-            })
-            mqtt.join('galaxy/service/shidur');
-            mqtt.join('galaxy/users/broadcast');
-            mqtt.send(JSON.stringify({type: "event", [user.role]: true}), true, 'galaxy/service/' + user.role);
-          }, 3000);
+  componentDidMount() {
+    setInterval(() => {
+      api.fetchProgram()
+        .then(qids => {
+          this.setState({qids});
+          if(this.state.qg) {
+            const {col, i} = this.state;
+            this.setState({qg: this.state.qids["q"+col].vquad[i]})
+          }
         })
-        const gateways = GxyJanus.makeGateways("rooms");
-        this.setState({gateways});
+        .catch(err => {
+          console.error("[SDIOut] error fetching quad state", err);
+          captureException(err, {source: "SDIOut"});
+        });
 
-        const gatewayToInitPromise = (gateway) => this.initGateway(user, gateway)
-					.catch(error => {
-						captureException(error, {source: 'SDIOut', gateway: gateway.name});
-						throw error;
-					});
+      api.fetchRoomsStatistics()
+        .then((roomsStatistics) => {
+          this.setState({roomsStatistics});
+        })
+        .catch(err => {
+          console.error("[SDIOut] error fetching rooms statistics", err);
+          captureException(err, {source: "SDIOut"});
+        });
+    }, 1000);
+    this.initApp();
+  };
 
-        return Promise.all(Object.values(gateways).map(gatewayToInitPromise))
-					.then(() => {
-						console.log("[SDIOut] gateways initialization complete");
-						this.setState({gatewaysInitialized: true});
-					});
-    };
+  componentWillUnmount() {
+    Object.values(this.state.gateways).forEach(x => x.destroy());
+  };
 
-    initGateway = (user, gateway) => {
-        console.log("[SDIOut] initializing gateway", gateway.name);
+  initApp = () => {
+    const {user} = this.state;
 
-        gateway.addEventListener("reinit", () => {
-                this.setState({reinit_inst: gateway.name});
-                this.postInitGateway(user, gateway)
-                    .catch(err => {
-                        console.error("[SDIOut] postInitGateway error after reinit. Reloading", gateway.name, err);
-                        captureException(err, {source: 'SDIOut', gateway: gateway.name});
-                        this.initGateway(user, gateway);
-                    });
-            }
-        );
+    api.setBasicAuth(API_BACKEND_USERNAME, API_BACKEND_PASSWORD);
 
-        return gateway.init()
-            .then(() => this.postInitGateway(user, gateway))
-            .catch(err => {
-                console.error("[SDIOut] error initializing gateway", gateway.name, err);
-                setTimeout(() => {
-                    this.initGateway(user, gateway)
-                        .catch(err => captureException(err, {source: 'SDIOut', gateway: gateway.name}));
-                }, 10000);
-            });
-    };
+    api.fetchConfig()
+      .then(data => GxyJanus.setGlobalConfig(data))
+      .then(() => this.initGateways(user))
+      .catch(err => {
+        console.error("[SDIOut] error initializing app", err);
+        this.setState({appInitError: err});
+        captureException(err, {source: 'SDIOut'});
+      });
+  };
 
-    postInitGateway = (user, gateway) => {
-        if (gateway.name === "gxy3") {
-            return gateway.initServiceProtocol(user, data => this.onServiceData(gateway, data));
-        }
-        return Promise.resolve();
-    };
+  initGateways = (user) => {
+    mqtt.init(user, (data) => {
+      console.log("[SDIOut] mqtt init: ", data)
+      setTimeout(() => {
+        mqtt.watch((data) => {
+          this.onMqttData(data);
+        })
+        mqtt.join('galaxy/service/shidur');
+        mqtt.join('galaxy/users/broadcast');
+        mqtt.send(JSON.stringify({type: "event", [user.role]: true}), true, 'galaxy/service/' + user.role);
+      }, 3000);
+    })
+    const gateways = GxyJanus.makeGateways("rooms");
+    this.setState({gateways});
+    Object.values(gateways).map(gateway => gateway.init())
+  };
 
   onMqttData = (data) => {
     const {room, col, feed, group, i, status, qst} = data;
@@ -176,140 +129,68 @@ class SDIOutApp extends Component {
     }
   };
 
-    onServiceData = (gateway, data, user) => {
-        const { gdm } = this.state;
-        if(GxyJanus.globalConfig.dynamic_config.galaxy_protocol === "mqtt") {
-          return
-        }
-        if (gdm.checkAck(data)) {
-          // Ack received, do nothing.
-          return;
-        }
-        gdm.accept(data, (msg) => gateway.sendServiceMessage(msg)).then((data) => {
-          if (data === null) {
-            console.log('Message received more then once.');
-            return;
-          }
+  reloadConfig = () => {
+    api.fetchConfig()
+      .then((data) => {
+        GxyJanus.setGlobalConfig(data);
+      })
+      .catch(err => {
+        console.error("[User] error reloading config", err);
+      });
+  }
 
-          if (data.type === "error") {
-            if (data.error_code === USERNAME_ALREADY_EXIST_ERROR_CODE) {
-              console.error("[SDIOut] service error message (reloading in 10 seconds)", data.error);
-              captureMessage(data.error, {source: "SDIOut", msg: data});
-              setTimeout(() => {
-                this.initGateway(user, gateway);
-              }, 10000);
-            } else {
-              captureException(data.error, {source: "SDIOut", msg: data});
-            }
-          }
+  render() {
+    let {vote,group,qids,qg,gateways, roomsStatistics} = this.state;
+    // let qst = g && g.questions;
+    let name = group && group.description;
 
-          const {room, col, feed, group, i, status, qst} = data;
-
-          if(data.type === "sdi-fullscr_group" && status) {
-              if(qst) {
-                  this.setState({col, i, group, room, qg: this.state.qids["q"+col].vquad[i]})
-              } else {
-                  this["col"+col].toFullGroup(i,feed);
-              }
-          } else if(data.type === "sdi-fullscr_group" && !status) {
-              let {col, feed, i} = data;
-              if(qst) {
-                  this.setState({group: null, room: null, qg: null});
-              } else {
-                  this["col"+col].toFourGroup(i,feed);
-              }
-          } else if(data.type === "sdi-vote") {
-              if(this.state.group)
-                  return
-              this.setState({vote: status, qg: null});
-          } else if(data.type === "sdi-restart_sdiout") {
-              window.location.reload();
-          } else if (data.type === 'reload-config') {
-            this.reloadConfig();
-          } else if(data.type === "event") {
-              delete data.type;
-              this.setState({...data});
-          }
-        }).catch((error) => {
-          console.error(`Failed receiving ${data}: ${error}`);
-        });
-    };
-
-    reloadConfig = () => {
-      api.fetchConfig()
-        .then((data) => {
-          GxyJanus.setGlobalConfig(data);
-        })
-        .catch(err => {
-          console.error("[User] error reloading config", err);
-        });
-    }
-
-    render() {
-        let {vote,appInitError, gatewaysInitialized,group,qids,qg,gateways, roomsStatistics} = this.state;
-        // let qst = g && g.questions;
-        let name = group && group.description;
-
-        if (appInitError) {
-        return (
-                <Fragment>
-                    <h1>Error Initializing Application</h1>
-                    {`${appInitError}`}
-                </Fragment>
-            );
-        }
-
-        if (!gatewaysInitialized) {
-            return "Initializing WebRTC gateways...";
-        }
-
-        return (
-            <Grid columns={2} className="sdi_container">
-                <Grid.Row>
-                    <Grid.Column>
-                        <UsersQuadSDIOut index={0} {...qids.q1} qst={qg} gateways={gateways}
-                                         roomsStatistics={roomsStatistics} ref={col => {this.col1 = col;}} />
-                    </Grid.Column>
-                    <Grid.Column>
-                        <UsersQuadSDIOut index={4} {...qids.q2} qst={qg} gateways={gateways}
-                                         roomsStatistics={roomsStatistics} ref={col => {this.col2 = col;}} />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column>
-                        <UsersQuadSDIOut index={8} {...qids.q3} qst={qg} gateways={gateways}
-                                         roomsStatistics={roomsStatistics} ref={col => {this.col3 = col;}} />
-                    </Grid.Column>
-                    <Grid.Column>
-                        <UsersQuadSDIOut index={12} {...qids.q4} qst={qg} gateways={gateways}
-                                         roomsStatistics={roomsStatistics} ref={col => {this.col4 = col;}} />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column>
-                        <Segment className="preview_sdi">
-                            <div className="usersvideo_grid">
-                                <div className="video_full">
-                                    {vote ?
-                                        <iframe title="Vote" src='https://vote.kli.one'
-                                                width="100%" height="100%" frameBorder="0" />
-                                    :
-                                        qg ? <Fragment>
-                                        {/*{group && group.questions ? <div className="qst_fullscreentitle">?</div> : ""}*/}
-                                        <div className="fullscrvideo_title" >{name}</div>
-                                        <UsersHandleSDIOut key={"q5"} g={qg} group={group} index={13} gateways={gateways} />
-                                        </Fragment> : ""
-                                    }
-                                </div>
-                            </div>
-                        </Segment>
-                    </Grid.Column>
-                    <Grid.Column>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        );
-    }
+    return (
+      <Grid columns={2} className="sdi_container">
+        <Grid.Row>
+          <Grid.Column>
+            <UsersQuadSDIOut index={0} {...qids.q1} qst={qg} gateways={gateways}
+                             roomsStatistics={roomsStatistics} ref={col => {this.col1 = col;}} />
+          </Grid.Column>
+          <Grid.Column>
+            <UsersQuadSDIOut index={4} {...qids.q2} qst={qg} gateways={gateways}
+                             roomsStatistics={roomsStatistics} ref={col => {this.col2 = col;}} />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <UsersQuadSDIOut index={8} {...qids.q3} qst={qg} gateways={gateways}
+                             roomsStatistics={roomsStatistics} ref={col => {this.col3 = col;}} />
+          </Grid.Column>
+          <Grid.Column>
+            <UsersQuadSDIOut index={12} {...qids.q4} qst={qg} gateways={gateways}
+                             roomsStatistics={roomsStatistics} ref={col => {this.col4 = col;}} />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <Segment className="preview_sdi">
+              <div className="usersvideo_grid">
+                <div className="video_full">
+                  {vote ?
+                    <iframe title="Vote" src='https://vote.kli.one'
+                            width="100%" height="100%" frameBorder="0" />
+                    :
+                    qg ? <Fragment>
+                      {/*{group && group.questions ? <div className="qst_fullscreentitle">?</div> : ""}*/}
+                      <div className="fullscrvideo_title" >{name}</div>
+                      <UsersHandleSDIOut key={"q5"} g={qg} group={group} index={13} gateways={gateways} />
+                    </Fragment> : ""
+                  }
+                </div>
+              </div>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
 
 export default SDIOutApp;
