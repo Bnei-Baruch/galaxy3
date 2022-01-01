8d19d44a2d93d38ee38800addf7c71654cd5473b
amnonbb <amnonbb@gmail.com>
2022-01-01 13:33:12 +0200

    Disable test myself button if no mic detected

--- src/apps/VirtualApp/VirtualClient.js
+++ src/apps/VirtualApp/VirtualClient.js
@@ -86,7 +86,7 @@ const userFeeds = (feeds) => feeds.filter((feed) => feed.display.role === userRo
 
 //for test server
 // const isUseNewDesign = new URL(window.location.href).searchParams.has('new_design');
-// const isUseNewDesign = true;
+//const isUseNewDesign = true;
 const isUseNewDesign = /arvut/.test(window.location.host);
 
 class VirtualClient extends Component {
