3be1180f5f302c3de13031e770b2461aee4f13b0
amnonbb <amnonbb@gmail.com>
2022-02-19 13:29:53 +0200

    VideoHandleMqtt: try using hooks

--- src/apps/VideoOutApp/VideoHandleMqtt.js
+++ src/apps/VideoOutApp/VideoHandleMqtt.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useRef, useEffect, useState} from "react";
 import "./VideoHandle.scss";
 import classNames from "classnames";
 import log from "loglevel";
@@ -7,94 +7,119 @@ import {PublisherPlugin} from "../../lib/publisher-plugin";
 import {SubscriberPlugin} from "../../lib/subscriber-plugin";
 import chalk from "chalk";
 
-class VideoHandleMqtt extends Component {
-  state = {
-    feeds: [],
-    inst: null,
-    mids: [],
-    name: "",
-    room: "",
-    num_videos: 0,
-    remoteFeed: null,
-    janus: null,
-    videoroom: null,
-    subscriber: null
+const VideoHandleMqtt = (props) => {
+  // state = {
+  //   feeds: [],
+  //   inst: null,
+  //   mids: [],
+  //   name: "",
+  //   room: "",
+  //   vcount: 0,
+  //   remoteFeed: null,
+  //   janus: null,
+  //   videoroom: null,
+  //   subscriber: null
+  //
+  // };
 
-  };
+  const {g, index, group, gateways, user, q, col} = props;
 
-  componentDidMount() {
-    let {g} = this.props;
-    let num_videos = g?.users?.filter((u) => u.camera).length;
-    if (num_videos > 25) num_videos = 25;
-    this.setState({num_videos});
-  }
+  const [vcount, setVcount] = useState(0);
+  const [feeds, setFeeds] = useState([]);
+  const [janus, setJanus] = useState(null);
 
-  componentDidUpdate(prevProps) {
-    let {g, index, group} = this.props;
-    const {room} = this.state;
-    if (g && index === 13 && g.room !== room && group) {
-      log.info(chalk.magenta("[VideoHandleMqtt] First IF : ", g, index, g.room, room, group));
-      this.setState({room: g.room}, () => {
-        this.initVideoRoom(g.room, g.janus);
-      });
+  let mit = "col" + col + "_q" + (q+1) + "_"
+  let videoroom = null
+  let subscriber = null
+  let room = null
+  let mids = []
+
+  useEffect(() => {
+    let {g} = props;
+    let vcount = g?.users?.filter((u) => u.camera).length;
+    if (vcount > 25) setVcount(25)
+    setVcount(vcount)
+  }, [g]);
+
+  useEffect(() => {
+    log.info(g)
+    if(g && g?.room !== room) {
+      room = g.room
+      log.info(chalk.magenta("[VideoHandleMqtt] First IF : ", g, index, g.room, group));
+      initVideoRoom(g.room, g.janus);
+      return () => {
+        exitVideoRoom(g.room, () => {});
+      }
     }
-    if (g && g.room !== room && index !== 13) {
-      log.info(chalk.magenta("[VideoHandleMqtt] Second IF : ", g, index, g.room, room));
-      this.setState({room: g.room}, () => {
-        if (room) {
-          this.exitVideoRoom(room, () => {
-            this.initVideoRoom(g.room, g.janus);
-          });
-        } else {
-          this.initVideoRoom(g.room, g.janus);
-        }
-      });
-    }
-    if (g && g.users && JSON.stringify(g) !== JSON.stringify(prevProps.g)) {
-      let num_videos = g.users.filter((u) => u.camera && u.role === "user").length;
-      if (num_videos > 25) num_videos = 25;
-      this.setState({num_videos});
-    }
-  }
+  }, [g && g?.room]);
 
-  componentWillUnmount() {
-    this.exitVideoRoom(this.state.room, () => {});
-  }
+  // componentDidMount() {
+  //   let {g} = this.props;
+  //   let vcount = g?.users?.filter((u) => u.camera).length;
+  //   if (vcount > 25) vcount = 25;
+  //   this.setState({vcount});
+  // }
+  //
+  // componentDidUpdate(prevProps) {
+  //   let {g, index, group} = this.props;
+  //   const {room} = this.state;
+  //   if (g && index === 13 && g.room !== room && group) {
+  //     log.info(chalk.magenta("[VideoHandleMqtt] First IF : ", g, index, g.room, room, group));
+  //     this.setState({room: g.room}, () => {
+  //       this.initVideoRoom(g.room, g.janus);
+  //     });
+  //   }
+  //   if (g && g.room !== room && index !== 13) {
+  //     log.info(chalk.magenta("[VideoHandleMqtt] Second IF : ", g, index, g.room, room));
+  //     this.setState({room: g.room}, () => {
+  //       if (room) {
+  //         this.exitVideoRoom(room, () => {
+  //           this.initVideoRoom(g.room, g.janus);
+  //         });
+  //       } else {
+  //         this.initVideoRoom(g.room, g.janus);
+  //       }
+  //     });
+  //   }
+  //   if (g && g.users && JSON.stringify(g) !== JSON.stringify(prevProps.g)) {
+  //     let vcount = g.users.filter((u) => u.camera && u.role === "user").length;
+  //     if (vcount > 25) vcount = 25;
+  //     this.setState({vcount});
+  //   }
+  // }
+  //
+  // componentWillUnmount() {
+  //   this.exitVideoRoom(this.state.room, () => {});
+  // }
 
-  initVideoRoom = (room, inst) => {
-    const {gateways, user, q, col} = this.props;
+  const initVideoRoom = (roomid, inst) => {
+    room = roomid
     let janus = gateways[inst];
-    const mit = "col" + col + "_q" + (q+1) + "_" + inst
+    mit = mit + inst
 
     log.info("["+mit+"] Init room: ", room, inst, ConfigStore.globalConfig)
     log.info("["+mit+"] mit", mit)
 
-    this.setState({mit, janus});
+    setJanus(janus)
 
-    this.initVideoHandles(janus, room, user)
-  }
-
-  initVideoHandles = (janus, room, user, mit) => {
-    let videoroom = new PublisherPlugin();
-    videoroom.subTo = this.onJoinFeed;
-    videoroom.unsubFrom = this.unsubscribeFrom
-    videoroom.talkEvent = this.handleTalking
+    videoroom = new PublisherPlugin();
+    videoroom.subTo = onJoinFeed;
+    videoroom.unsubFrom = unsubscribeFrom
+    videoroom.talkEvent = handleTalking
 
     janus.attach(videoroom).then(data => {
       log.info("["+mit+"] Publisher Handle: ", data)
 
       videoroom.join(room, user).then(data => {
         log.info("["+mit+"] Joined respond :", data)
-        this.setState({videoroom, user, room, remoteFeed: null});
-        this.onJoinMe(data.publishers, room)
+        onJoinMe(data.publishers, room)
       }).catch(err => {
         log.error("["+mit+"] Join error :", err);
       })
     })
   }
 
-  onJoinMe = (list, room) => {
-    const {mit} = this.state;
+  const onJoinMe = (list, room) => {
     let feeds = list
       .sort((a, b) => JSON.parse(a.display).timestamp - JSON.parse(b.display).timestamp)
       .filter((feeder) => JSON.parse(feeder.display).role === "user");
@@ -116,14 +141,13 @@ class VideoHandleMqtt extends Component {
         }
       }
     }
-    this.setState({feeds});
+    setFeeds(feeds);
     if (subscription.length > 0) {
-      this.subscribeTo(room, subscription);
+      subscribeTo(room, subscription);
     }
   }
 
-  onJoinFeed = (feed) => {
-    let {feeds, room, mit} = this.state;
+  const onJoinFeed = (feed) => {
     log.info("["+mit+"] Feed enter: ", feeds);
     let subscription = [];
     for (let f in feed) {
@@ -144,124 +168,115 @@ class VideoHandleMqtt extends Component {
     const isExistFeed = feeds.find((f) => f.id === feed[0].id);
     if (!isExistFeed) {
       feeds.push(feed[0]);
-      this.setState({feeds});
+      setFeeds(feeds);
     }
     if (subscription.length > 0) {
-      this.subscribeTo(room, subscription);
+      subscribeTo(room, subscription);
     }
   }
 
-  cleanState = (callback) => {
-    this.setState({feeds: [], mids: [], remoteFeed: false, videoroom: null, subscriber: null, janus: null});
+  const cleanState = (callback) => {
+    setFeeds([]);
+    setJanus(null)
+    videoroom = null
+    subscriber = null
+    mids = []
+    //this.setState({feeds: [], mids: [], remoteFeed: false, videoroom: null, subscriber: null, janus: null});
     if(typeof callback === "function") callback();
   }
 
-  exitVideoRoom = (roomid, callback) => {
-    const {janus, videoroom, mit} = this.state;
+  const exitVideoRoom = (roomid, callback) => {
     if(videoroom) {
       videoroom.leave().then(r => {
         log.info("["+mit+"] leave respond:", r);
         janus.detach(videoroom).then(() => {
           log.info("["+mit+"] plugin detached:");
-          this.cleanState(callback)
+          cleanState(callback)
         })
       }).catch(e => {
         log.error("["+mit+"] leave error:", e);
-        this.cleanState(callback)
+        cleanState(callback)
       });
     } else {
-      this.cleanState(callback)
+      cleanState(callback)
     }
 
   };
 
-  subscribeTo = (room, subscription) => {
-    let {janus, creatingFeed, remoteFeed, subscriber, mit} = this.state
+  const subscribeTo = (room, subscription) => {
 
-    if (remoteFeed) {
+    if (subscriber) {
       subscriber.sub(subscription);
       return;
     }
 
-    if (creatingFeed) {
-      setTimeout(() => {
-        this.subscribeTo(subscription);
-      }, 500);
-      return;
-    }
+    // if (creatingFeed) {
+    //   setTimeout(() => {
+    //     subscribeTo(subscription);
+    //   }, 500);
+    //   return;
+    // }
 
     subscriber = new SubscriberPlugin();
-    subscriber.onTrack = this.onRemoteTrack;
-    subscriber.onUpdate = this.onUpdateStreams;
+    subscriber.onTrack = onRemoteTrack;
+    subscriber.onUpdate = onUpdateStreams;
 
     janus.attach(subscriber).then(data => {
-      this.setState({subscriber});
       log.info("["+mit+"] Subscriber Handle: ", data)
       subscriber.join(subscription, room).then(data => {
         log.info("["+mit+"] Subscriber join: ", data)
-        this.onUpdateStreams(data.streams);
-        this.setState({remoteFeed: true, creatingFeed: false});
+        onUpdateStreams(data.streams);
       });
     })
   };
 
-  unsubscribeFrom = (id) => {
+  const unsubscribeFrom = (id) => {
     id = id[0]
-    const {feeds, subscriber, mit} = this.state;
     log.info("["+mit+"] unsubscribeFrom", id);
     for (let i = 0; i < feeds.length; i++) {
       if (feeds[i].id === id) {
         log.info("["+mit+"] unsubscribeFrom feed", feeds[i]);
-
         feeds.splice(i, 1);
-
         const streams = [{feed: id}]
-
-        const {remoteFeed} = this.state;
-        if (remoteFeed !== null && streams.length > 0) {
+        if (streams.length > 0) {
           subscriber.unsub(streams);
         }
-
-        this.setState({feeds});
+        setFeeds(feeds);
         break;
       }
     }
   };
 
-  handleTalking = (id, talking) => {
-    const feeds = Object.assign([], this.state.feeds);
+  const handleTalking = (id, talking) => {
     for (let i = 0; i < feeds.length; i++) {
       if (feeds[i] && feeds[i].id === id) {
         feeds[i].talking = talking;
       }
     }
-    this.setState({feeds});
+    setFeeds(feeds);
   }
 
-  onUpdateStreams = (streams) => {
-    const mids = Object.assign([], this.state.mids);
+  const onUpdateStreams = (streams) => {
     for (let i in streams) {
       let mindex = streams[i]["mid"];
       //let feed_id = streams[i]["feed_id"];
       mids[mindex] = streams[i];
     }
-    this.setState({mids});
   }
 
-  onRemoteTrack = (track, mid, on) => {
-    let {mids} = this.state;
+  const onRemoteTrack = (track, mid, on) => {
+    log.info("got track", track)
     let feed = mids[mid].feed_id;
     if (track.kind === "video" && on) {
       let stream = new MediaStream();
       stream.addTrack(track.clone());
-      let remotevideo = this.refs["pv" + feed];
-      if (remotevideo) remotevideo.srcObject = stream;
+      feed = useRef();
+      log.info(feed.current)
+      if (feed.current.srcObject) feed.current.srcObject = stream;
     }
   }
 
-  render() {
-    const {feeds, num_videos} = this.state;
-    const {g} = this.props;
+  const render = () => {
     const width = "400";
     const height = "300";
     const autoPlay = true;
@@ -269,7 +284,7 @@ class VideoHandleMqtt extends Component {
     const muted = true;
     //const q = (<b style={{color: "red", fontSize: "20px", fontFamily: "Verdana", fontWeight: "bold"}}>?</b>);
 
-    let program_feeds = feeds.map((feed) => {
+    return (feeds.map((feed) => {
       let camera = g && g.users && !!g.users.find((u) => feed.id === u.rfid && u.camera);
       if (feed) {
         let id = feed.id;
@@ -286,7 +301,7 @@ class VideoHandleMqtt extends Component {
             </div>
             <video
               key={id}
-              ref={"pv" + id}
+              ref={id}
               id={"pv" + id}
               width={width}
               height={height}
@@ -299,18 +314,19 @@ class VideoHandleMqtt extends Component {
         );
       }
       return true;
-    });
+    }))
+  }
 
     return (
-      <div className={`vclient__main-wrapper no-of-videos-${num_videos} layout--equal broadcast--off`}>
+      <div className={`vclient__main-wrapper no-of-videos-${vcount} layout--equal broadcast--off`}>
         <div className="videos-panel">
           <div className="videos">
-            <div className="videos__wrapper">{program_feeds}</div>
+            <div className="videos__wrapper">{render()}</div>
           </div>
         </div>
       </div>
     );
-  }
+
 }
 
 export default VideoHandleMqtt;
