0cd48634faab938edc53b29f2f209533d7c33c07
amnonbb <amnonbb@gmail.com>
2022-09-14 10:48:27 +0300

    Bump version

--- src/Version.js
+++ src/Version.js
@@ -1,2 +1,2 @@
 // export default `5.1.7${process.env.NODE_ENV === "development" ? "-dev" : ""}`;
-export default `6.1.0${process.env.NODE_ENV === "development" ? "-dev" : "-test"}`;
+export default `6.1.1${process.env.NODE_ENV === "development" ? "-dev" : ""}`;
