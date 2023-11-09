a87337fff890a9b3a95434fd60b4d34b9359acac
amnonbb <amnonbb@gmail.com>
2023-11-09 17:21:15 +0200

    Make language tabs on notification message

--- src/apps/VirtualApp/components/BroadcastNotification.js
+++ src/apps/VirtualApp/components/BroadcastNotification.js
@@ -4,14 +4,20 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import {LANG_MAP} from "../../../shared/consts";
 
 export const getLanguage = () => LANG_MAP[localStorage.getItem("vrt_langtext")] || "en";
-
+const lang = getLanguage()
 export const BroadcastNotification = ({show, msg, setClose}) => {
+    const [value, setValue] = React.useState(lang);
 
-    const lang = getLanguage()
-    const message = msg[lang]
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <div>
@@ -22,12 +28,20 @@ export const BroadcastNotification = ({show, msg, setClose}) => {
                 aria-describedby="alert-dialog-description"
                 style={{zIndex:1301}}
             >
-                {/*<DialogTitle id="alert-dialog-title">*/}
-                {/*    {"Arvut System Notification"}*/}
-                {/*</DialogTitle>*/}
                 <DialogContent>
                     <DialogContentText id="alert-dialog-description">
-                        <div dangerouslySetInnerHTML={{__html: message}}></div>
+                        <TabContext value={value}>
+                            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                <Tab label="English" value="en" />
+                                <Tab label="Russian" value="ru" />
+                                <Tab label="Hebrew" value="he" />
+                                <Tab label="Spanish" value="es" />
+                            </TabList>
+                            <TabPanel className="notification" value="en"><div dangerouslySetInnerHTML={{__html: msg[value]}}></div></TabPanel>
+                            <TabPanel className="notification" value="ru"><div dangerouslySetInnerHTML={{__html: msg[value]}}></div></TabPanel>
+                            <TabPanel className="notification" value="he"><div dangerouslySetInnerHTML={{__html: msg[value]}}></div></TabPanel>
+                            <TabPanel className="notification" value="es"><div dangerouslySetInnerHTML={{__html: msg[value]}}></div></TabPanel>
+                        </TabContext>
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
