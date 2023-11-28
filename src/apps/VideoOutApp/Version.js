ab78a272e5d703b36bb2f4ff0763b313b6a27240
amnonbb <amnonbb@gmail.com>
2023-11-28 14:25:20 +0200

    Fix error with feeds.desktop

--- src/apps/VideoOutApp/Version.js
+++ src/apps/VideoOutApp/Version.js
@@ -1,1 +1,1 @@
-export default `1.1.2${process.env.NODE_ENV === "development" ? "-dev" : ""}`;
+export default `1.1.3${process.env.NODE_ENV === "development" ? "-dev" : ""}`;
--- src/apps/VideoOutApp/VideoHandleMqtt.js
+++ src/apps/VideoOutApp/VideoHandleMqtt.js
@@ -247,11 +247,11 @@ class VideoHandleMqtt extends Component {
   handleTalking = (id, talking) => {
     const feeds = Object.assign([], this.state.feeds);
     for (let i = 0; i < feeds.length; i++) {
-      if (feeds[i] && feeds[i].id === id && feeds[i].display?.is_desktop) {
+      if (feeds[i] && feeds[i].id === id && feeds[i]?.display?.is_desktop) {
         feeds[i].talking = talking;
+        this.setState({feeds});
       }
     }
-    this.setState({feeds});
   }
 
   onUpdateStreams = (streams) => {
