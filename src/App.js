8eff46003a66eadd4be8e38d8ea6da7620d75620
amnonbb <amnonbb@gmail.com>
2023-12-04 15:25:21 +0200

    AudioOutApp: fix duplicate feeds

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import i18n from "i18next";
 
 import "./i18n/i18n";
 // import GalaxyApp from "./apps/GalaxyApp";
-import VirtualMqttClient from "./apps/VirtualApp/VirtualMqttClient";
+// import VirtualMqttClient from "./apps/VirtualApp/VirtualMqttClient";
 // import AdminApp from "./apps/AdminApp/AdminApp";
 // import ShidurAppMqtt from "./apps/ShidurApp/ShidurAppMqtt";
 // import ShidurAppHttp from "./apps/ShidurApp/ShidurAppHttp";
-// import AudioOutMqtt from "./apps/AudioOutApp/AudioOutMqtt";
+import AudioOutMqtt from "./apps/AudioOutApp/AudioOutMqtt";
 // import VideoOutMqtt from "./apps/VideoOutApp/VideoOutMqtt";
 // import QstOutMqtt from "./apps/VideoOutApp/QstOutMqtt";
 // import WebOutMqtt from "./apps/WebOutApp/WebOutMqtt";
@@ -19,11 +19,11 @@ class App extends Component {
     return (
       <I18nextProvider i18n={i18n}>
         {/*{<GalaxyApp />}*/}
-        <VirtualMqttClient />
+        {/*<VirtualMqttClient />*/}
         {/*<AdminApp />*/}
         {/*<ShidurAppMqtt />*/}
         {/*<ShidurAppHttp />*/}
-        {/*<AudioOutMqtt />*/}
+        <AudioOutMqtt />
         {/*<VideoOutMqtt />*/}
         {/*<QstOutMqtt />*/}
         {/*<WebOutMqtt />*/}
