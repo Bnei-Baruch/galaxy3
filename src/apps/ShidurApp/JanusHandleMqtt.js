55f895029c69a816b11dea647d93005ba41a61b7
amnonbb <amnonbb@gmail.com>
2024-07-21 11:10:57 +0300

    Clean up video before detach plugins

--- src/apps/ShidurApp/JanusHandleMqtt.js
+++ src/apps/ShidurApp/JanusHandleMqtt.js
@@ -137,15 +137,7 @@ class JanusHandleMqtt extends Component {
   }
 
   exitPlugins = (callback) => {
-    const {subscriber, videoroom, janus, mit, feeds} = this.state;
-    feeds.forEach(f => {
-      let e = this.refs["pv" + f.id];
-      if (e) {
-        e.src = "";
-        e.srcObject = null;
-        e.remove();
-      }
-    })
+    const {subscriber, videoroom, janus, mit} = this.state;
     if(janus) {
       if(subscriber) janus.detach(subscriber)
       janus.detach(videoroom).then(() => {
@@ -157,7 +149,15 @@ class JanusHandleMqtt extends Component {
   }
 
   exitVideoRoom = (roomid, callback) => {
-    const {videoroom, mit} = this.state;
+    const {videoroom, mit, feeds} = this.state;
+    feeds.forEach(f => {
+      let e = this.refs["pv" + f.id];
+      if (e) {
+        e.src = "";
+        e.srcObject = null;
+        e.remove();
+      }
+    })
     if(videoroom) {
       videoroom.leave().then(r => {
         log.info("["+mit+"] leave respond:", r);
