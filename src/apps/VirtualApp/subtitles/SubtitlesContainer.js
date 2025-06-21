8c10ac298f04066d3623bb2e4a02aa15324091c6
bbfsdev <bbfsdev@gmail.com>
2025-06-21 02:38:03 +0000

    Fix subtitles & questions.

--- src/apps/VirtualApp/subtitles/SubtitlesContainer.js
+++ src/apps/VirtualApp/subtitles/SubtitlesContainer.js
@@ -19,9 +19,87 @@ export const SubtitlesContainer = ({playerLang, layout}) => {
     return () => messageManager.exit();
   }, [subLang, wqLang]);
 
+  let {msg: {message, isLtr, slide} = {}, language, wqLangs, display_status} = msgState;
+	let fakeSlide = '';
+	let fakeMsgState = {};
+
+	// In process
+	fakeMsgState = {
+		msg: {
+			slide: '',
+			message: '',
+			isLtr: false,
+		},
+		language: "he",
+		wqLangs: ['en', 'es', 'he', 'ru'],
+		display_status: "questions",
+	}
+
+
+	// Question
+  fakeSlide = '<p>סיכום שיעור בעשיריות.</p>';
+	fakeMsgState = {
+		msg: {
+			slide: '', // fakeSlide,
+			message: '', // fakeSlide,
+			isLtr: false,
+		},
+		language: "he",
+		wqLangs: ['en', 'es', 'he', 'ru'],
+		display_status: "questions",
+	}
+
+	// Link - Have not checked yet.
+	fakeSlide = '<a href="kuku">this is a link</a>';
+	fakeMsgState = {
+		msg: {
+			slide: fakeSlide,
+			message: fakeSlide,
+			isLtr: false,
+		},
+		language: "he",
+		wqLangs: [],
+		display_status: "subtitles",
+	}
+
+	// Subtitle
+	fakeSlide = "<p>2. על כל דבר שמקבלים צריכים להקדים תפלה, שיהיה כלי לקבל את ההשפעה, יוצא, שאפילו לאחר מה שהקדוש ברוך הוא הבטיחו במראה הסולם, זה נקרא אור מקיף.</p><p>אבל בזמן שפגש את עשו, שאז הוצרך לישועה בהוה,</p>";
+	fakeMsgState = {
+		msg: {
+			slide: fakeSlide,
+			message: fakeSlide,
+			isLtr: false,
+		},
+		language: "he",
+		wqLangs: ['en', 'es', 'he', 'ru'],
+		display_status: "subtitles",
+	}
+
+
+	// wqLangs = [];
+  // Hebrew subtitle
+	/*display_status = "subtitles";
+	slide = "";
+  isLtr = false;
+	// Hebrew question
+	display_status = "questions";
+	slide = "<p>תסכמו את השיעור בעשיריות. תסכמו את השיעור בעשיריות. תסכמו את השיעור בעשיריות. תסכמו את השיעור בעשיריות.</p>";
+  isLtr = false;
+  // English subtitle
+	display_status = "subtitles";
+	slide = '<p>5) The number thirteen: the thirteen qualities of mercy or upper KHB HGT, and lower HGT and NHYM.</p>' +
+		'<p>We must understand: It is said in <em>Sefer</em> <em>Yetzira</em> [<em>The Book of Creation</em>], “Ten and not nine; ten and not eleven.” Thus,</p>';
+	// English question
+	display_status = "questions";
+	slide = '<p>Summarize the lesson in the ten. Summarize the lesson in the ten. Summarize the lesson in the ten. Summarize the lesson in the ten.</p>';
+	isLtr = true;
+	message = slide;
+	*/
+
 
   if (!msgState || msgState.display_status === MSGS_NONE.display_status)
     return null;
 
   return <SubtitlesView msgState={msgState}/>
+  // return <SubtitlesView msgState={fakeMsgState}/>
 };
