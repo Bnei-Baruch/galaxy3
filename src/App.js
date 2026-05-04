30af53bee807c48cc7782571bbf8406e186fb75f
amnonbb <amnonbb@gmail.com>
2026-05-04 11:49:11 +0300

    Add dynamic import

--- src/App.js
+++ src/App.js
@@ -1,54 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
 
 import './i18n/i18n';
-import GalaxyApp from './apps/GalaxyApp';
-import VirtualMqttClient from './apps/VirtualApp/VirtualMqttClient';
-import AdminApp from './apps/AdminApp/AdminApp';
-import ShidurAppMqtt from './apps/ShidurApp/ShidurAppMqtt';
-import AudioOutMqtt from './apps/AudioOutApp/AudioOutMqtt';
-import VideoOutMqtt from './apps/VideoOutApp/VideoOutMqtt';
-import QstOutMqtt from './apps/VideoOutApp/QstOutMqtt';
-import WebOutMqtt from './apps/WebOutApp/WebOutMqtt';
+
+const apps = {
+  galaxy: lazy(() => import('./apps/GalaxyApp')),
+  virtual: lazy(() => import('./apps/VirtualApp/VirtualMqttClient')),
+  admin: lazy(() => import('./apps/AdminApp/AdminApp')),
+  shidur: lazy(() => import('./apps/ShidurApp/ShidurAppMqtt')),
+  audioout: lazy(() => import('./apps/AudioOutApp/AudioOutMqtt')),
+  videoout: lazy(() => import('./apps/VideoOutApp/VideoOutMqtt')),
+  qstout: lazy(() => import('./apps/VideoOutApp/QstOutMqtt')),
+  webout: lazy(() => import('./apps/WebOutApp/WebOutMqtt')),
+};
 
 class App extends Component {
   render() {
     const appName = process.env.REACT_APP_GALAXY_APP || 'virtual';
-    let AppComponent;
-
-    switch (appName) {
-      case 'galaxy':
-        AppComponent = GalaxyApp;
-        break;
-      case 'admin':
-        AppComponent = AdminApp;
-        break;
-      case 'shidur':
-        AppComponent = ShidurAppMqtt;
-        break;
-      case 'audioout':
-        AppComponent = AudioOutMqtt;
-        break;
-      case 'videoout':
-        AppComponent = VideoOutMqtt;
-        break;
-      case 'qstout':
-        AppComponent = QstOutMqtt;
-        break;
-      case 'webout':
-        AppComponent = WebOutMqtt;
-        break;
-      case 'virtual':
-      default:
-        AppComponent = VirtualMqttClient;
-        break;
-    }
+    const AppComponent = apps[appName] || apps.virtual;
 
     return (
       <I18nextProvider i18n={i18n}>
-        <AppComponent />
+        <Suspense fallback={null}>
+          <AppComponent />
+        </Suspense>
       </I18nextProvider>
     );
   }
